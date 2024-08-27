22cee5f8def00f2cdff4a8412c4881615f35228c
sibas1 <117487764+sibas1@users.noreply.github.com>
2024-08-27 03:58:30 -0500

    commit

--- src/app.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import morgan from "morgan";
-
-const app = express();
-app.use(morgan("dev"))
-export default app;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import app from "./app.js"
-import { conecDB } from "./db.js"
-
-conecDB();
-app.listen(3000)
-console.log("server on 3000")
