ee08e7f8c408362156da05b06a095ee9b1baf6f2
Luis Reyes <yuiftw05@gmail.com>
2024-08-28 03:57:18 -0600

    feat: redux set up done and working(comments added)

--- client/src/redux/slices/userSlice.ts
+++ client/src/redux/slices/userSlice.ts
@@ -9,6 +9,7 @@ const userSlice = createSlice({
     // el nombre que usaremos para llamar al estado global de este slice y ver sus props
     name: "user",
     initialState,
+    //aca los distintos seteadores del estado, que usaremos para cambiar el valor de lo que ocupemos, con lo que recibimos por payload
     reducers: {
         updateCounter: (state,action)=> {
             state.counter = state.counter + action.payload
@@ -17,4 +18,5 @@ const userSlice = createSlice({
 })
 
 export const {updateCounter} = userSlice.actions
+// este export es para poder usarlo en la store
 export default userSlice.reducer
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -16,7 +16,12 @@ const store = configureStore({
         [authApi.reducerPath]: authApi.reducer,
         [userApi.reducerPath]: userApi.reducer,
     },
+    //concatenamos los apiSlices que usamos para hacer las peticiones a nuestro backend
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware, authApi.middleware)
 })
+
+// typescript necesita el root state para poder saber que tipo de valores estamos asignando en nuestro estado global, de otra forma, no podra leerlos
+export type RootState = ReturnType<typeof store.getState>
+export type appDispatch = typeof store.dispatch
 
 export default store;
