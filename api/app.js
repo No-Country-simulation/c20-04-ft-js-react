12371e6f7e0b5798991ecea4996968b85fb41aec
Luis Reyes <yuiftw05@gmail.com>
2024-08-30 02:15:57 -0600

    feat: instalando cors y exportandolo en app.js

--- api/app.js
+++ api/app.js
@@ -2,9 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import authrouter from "./routers/auth.routers.js";
+import cors from 'cors'
 
 
 const app = express();
+
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}))
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
