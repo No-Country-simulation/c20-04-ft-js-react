1c44aa0d6b5fbd6ca3780939f5a1830a203a1800
Luis Reyes <yuiftw05@gmail.com>
2024-08-31 08:47:16 -0600

    feat: primera parte del profile terminada(sin opciones para mostrar)

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -6,6 +6,15 @@ interface profileProps {
 
 export default function Profile({username}: profileProps) {
   return (
-    <div>welcome to your profile user {username}</div>
+    <div className="max-w-7xl w-9/10 mx-auto">
+        <div className="flex gap-4 justify-end border-b-[4px] border-b-[#334155] align-center">
+        <p>@{username}</p>
+        <div className="">...</div>
+        {/* linea que divide la primera parte del perfil */}
+      <div className="border-b-[4px] border-b-[#334155] mt-6"></div>
+      </div>
+
+      {/* segunda parte del perfil */}
+    </div>
   )
 }
