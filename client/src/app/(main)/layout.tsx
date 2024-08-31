a0b6221e9ded36922fbbee9c61a9fd4924ac6746
Dagadev <gacub20ru@gmail.com>
2024-08-31 14:27:16 -0600

    Add main route group & Add main layout

---
+++ client/src/app/(main)/layout.tsx
@@ -0,0 +1,22 @@
+import Header from '@/components/header/Header'
+import SideNavBar from '@/components/shared/SideNavBar'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | PawPal',
+    default: 'PawPal'
+  }
+}
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className='min-h-dvh max-w-screen-xl mx-auto flex flex-col'>
+      <Header />
+      <div className='flex flex-1 flex-col md:flex-row relative'>
+        <SideNavBar />
+        {children}
+      </div>
+    </main >
+  )
+}
