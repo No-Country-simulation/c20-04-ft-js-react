f93aab962a9b272b970b4c90b063752f71b5b5a6
luciano-repetti <lucianorepetti2020@gmail.com>
2024-08-31 02:08:55 -0300

    new header with components material UI

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,30 +1,33 @@
-// Por convención, los nombres de componentes de React usan PascalCase
+"use client"
+
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Avatar, Box, ThemeProvider } from '@mui/material';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { useTheme } from '@mui/material/styles';
+
 export default function Header() {
+    const theme = useTheme();
+
     return (
-        <header className="flex flex-row border-2 border-neutral-300 w-full items-center justify-between">
-            <div className="flex flex-row items-center">
-                <p className="bg-neutral-400 rounded-full text-xl mx-8 p-2 my-2">Logo</p>
-                <p className="text-2xl">PawPal Community</p>
-            </div>
-            <div className="flex flex-row items-center">
-                <p className="bg-neutral-400 rounded-full text-xl mr-4 p-2 my-2">Foto</p>
-                <p className="text-2xl mr-3">Nombre de usuario</p>
-                <button>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        className="icon icon-tabler icons-tabler-filled icon-tabler-triangle-inverted h-4 w-4 mr-8">
-                        <path
-                            stroke="none"
-                            d="M0 0h24v24H0z"
-                            fill="none"
-                        />
-                        <path
-                            d="M20.118 3h-16.225a2.914 2.914 0 0 0 -2.503 4.371l8.116 13.549a2.917 2.917 0 0 0 4.987 .005l8.11 -13.539a2.914 2.914 0 0 0 -2.486 -4.386z" />
-                    </svg>
-                </button>
-            </div>
-        </header>
-    )
+        <AppBar position="static" color="default" elevation={0} sx={{ backgroundColor: "#fff", border: '1px solid #e0e0e0', borderTop: "none" }}>
+            <Toolbar sx={{ justifyContent: 'space-between' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    {/* <Box>Imagen del logo de PawPañ Community</Box> */}
+                    <Typography variant="h1" noWrap component="div" sx={{ fontSize: { xs: "1.5em", sm: "1.8em" }, display: "block" }}>
+                        PawPal Community
+                    </Typography>
+                </Box>
+
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <IconButton color='inherit' sx={{ mr: 1 }}>
+                        <NotificationsIcon />
+                    </IconButton>
+                    <Avatar alt="Luucianorepetti" src="/placeholder.svg?height=32&width=32" sx={{ backgroundColor: theme.palette.primary.main, width: 40, height: 40, mr: 1 }} />
+                    <Typography variant="body1" noWrap component="div" sx={{ fontSize: "1.2em", display: { xs: 'none', sm: 'block' } }}>
+                        @luucianorepetti
+                    </Typography>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
 }
