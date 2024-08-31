a228eb7a3f19e6593f502599978decc57f8fa4d2
Dagadev <gacub20ru@gmail.com>
2024-08-31 10:13:53 -0600

    Add svg icons

--- client/src/components/shared/SideNavBar.tsx
+++ client/src/components/shared/SideNavBar.tsx
@@ -1,32 +1,34 @@
-import { IoHomeOutline } from 'react-icons/io5';
-import { CiUser, CiMail } from 'react-icons/ci';
-import { IoSettingsOutline, IoLogOutOutline } from 'react-icons/io5';
+import HomeIcon from "@/icons/HomeIcon";
+import LogOutIcon from "@/icons/LogOutIcon";
+import MailIcon from "@/icons/MailIcon";
+import SettingsIcon from "@/icons/SettingsIcon";
+import UserIcon from "@/icons/UserIcon";
 
 const routes = [
   {
     name: 'Home',
     path: '/',
-    icon: IoHomeOutline
+    icon: HomeIcon
   },
   {
     name: 'Mensajes',
     path: '/messages',
-    icon: CiMail
+    icon: MailIcon
   },
   {
     name: 'Perfil',
     path: '/me',
-    icon: CiUser
+    icon: UserIcon
   },
   {
     name: 'Configuracíon',
     path: '/settings',
-    icon: IoSettingsOutline
+    icon: SettingsIcon
   },
   {
     name: 'Cerrar sesión',
     path: '/log-out',
-    icon: IoLogOutOutline
+    icon: LogOutIcon
   }
 ];
 
@@ -46,7 +48,7 @@ export default function SideNavBar() {
             href={route.path}
             className='flex gap-x-4 py-2 px-4 items-center font-semibold rounded-full border border-transparent transition text-black/60 hover:text-black hover:border-neutral-300'
           >
-            <route.icon className='size-8' />
+            <route.icon />
             <span>{route.name}</span>
           </a>
         ))}
---
+++ client/src/icons/LogOutIcon.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from 'react';
+
+export default function LogOutIcon({ ...props }: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      width='32'
+      height='32'
+      viewBox='0 0 32 32'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M19 21v2.5a2.5 2.5 0 0 1-2.5 2.5h-10A2.5 2.5 0 0 1 4 23.5v-15A2.5 2.5 0 0 1 6.5 6H16c1.38 0 3 1.12 3 2.5V11m4 10 5-5-5-5m-12 5h16'
+        stroke='#000'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  )
+}
