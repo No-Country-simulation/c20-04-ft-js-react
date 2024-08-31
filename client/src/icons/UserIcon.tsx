f6b4bf99bee3126aaf3b90650a53f484abf9a3fd
Dagadev <gacub20ru@gmail.com>
2024-08-31 11:27:56 -0600

    Fix props names

--- client/src/icons/UserIcon.tsx
+++ client/src/icons/UserIcon.tsx
@@ -13,15 +13,15 @@ export default function UserIcon({ ...props }: SVGProps<SVGSVGElement>) {
       <path
         d='M21.5 9c-.245 3.304-2.75 6-5.5 6s-5.26-2.695-5.5-6c-.25-3.438 2.188-6 5.5-6 3.313 0 5.75 2.625 5.5 6Z'
         stroke='#000'
-        stroke-width='2'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M16 19c-5.438 0-10.956 3-11.977 8.663C3.899 28.345 4.286 29 5 29h22c.715 0 1.101-.655.978-1.337C26.956 22 21.438 19 16 19Z'
         stroke='#000'
-        stroke-width='2'
-        stroke-miterlimit='10'
+        strokeWidth='2'
+        strokeMiterlimit='10'
       />
     </svg>
   );
