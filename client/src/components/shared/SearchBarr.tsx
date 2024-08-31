6ea0ed26058b6067abb73ede334cff6ba9b59225
Dagadev <gacub20ru@gmail.com>
2024-08-31 13:48:16 -0600

    Add SearchBarr component

---
+++ client/src/components/shared/SearchBarr.tsx
@@ -0,0 +1,9 @@
+export default function SearchBarr() {
+  return (
+    <input
+      className='border bg-neutral-100 dark:bg-neutral-800 border-neutral-300 dark:border-neutral-700 rounded-full py-2 px-4 mb-10 outline-none transition-colors focus:border-black dark:focus:border-white'
+      type='search'
+      placeholder='Buscar'
+    />
+  )
+}
