e64858a57c0ebb0b41e8b46406dcbc17dfd63462
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-09-01 16:26:47 -0500

    fixed theme for app

--- client/src/app/(main)/home/page.tsx
+++ client/src/app/(main)/home/page.tsx
@@ -1,17 +1,28 @@
-import ThemeSwitcher from '@/components/shared/ThemeSwitcher'
-import { Box } from '@mui/material'
-import type { Metadata } from 'next'
+"use client";
+import ThemeSwitcher from "@/components/shared/ThemeSwitcher";
+import { Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
-export const metadata: Metadata = {
-  title: 'Home'
-}
+// tuve que pasar a use client para que funcione el useTheme y al ser un hook de mui no se puede importar
+//import type { Metadata } from "next";
+// export const metadata: Metadata = {
+//   title: "Home",
+// };
 
 export default function Home() {
+  const theme = useTheme();
   return (
-    <Box p={5} gap={4} sx={{
-      display: 'flex', flexDirection: 'column', width: '100% ', borderRight: '1px solid #e0e0e0'
-    }}>
-      < ThemeSwitcher />
+    <Box
+      p={5}
+      gap={4}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100% ",
+        borderRight: `1px solid ${theme.palette.divider}`,
+      }}
+    >
+      <ThemeSwitcher />
     </Box>
-  )
+  );
 }
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,33 +1,76 @@
-"use client"
+"use client";
 
-import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Avatar, Box, ThemeProvider } from '@mui/material';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import { useTheme } from '@mui/material/styles';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Avatar,
+  Box,
+  ThemeProvider,
+} from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { useTheme } from "@mui/material/styles";
 
 export default function Header() {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    return (
-        <AppBar position="static" color="default" elevation={0} sx={{ height: "7svh", display: 'flex', justifyContent: "center", backgroundColor: "#fff", border: '1px solid #e0e0e0', borderTop: "none" }}>
-            <Toolbar sx={{ justifyContent: 'space-between' }}>
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    {/* <Box>Imagen del logo de PawPañ Community</Box> */}
-                    <Typography variant="h1" noWrap component="div" sx={{ fontSize: { xs: "1.5em", sm: "1.8em" }, display: "block" }}>
-                        PawPal Community
-                    </Typography>
-                </Box>
+  return (
+    <AppBar
+      position="static"
+      color="default"
+      elevation={0}
+      sx={{
+        height: "7svh",
+        display: "flex",
+        justifyContent: "center",
+        backgroundColor: theme.palette.background.paper,
+        border: `1px solid ${theme.palette.divider}`,
+        borderTop: "none",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          {/* <Box>Imagen del logo de PawPañ Community</Box> */}
+          <Typography
+            variant="h1"
+            noWrap
+            component="div"
+            sx={{
+              fontSize: { xs: "1.5em", sm: "1.8em" },
+              display: "block",
+              color: theme.palette.primary.main,
+            }}
+          >
+            PawPal Community
+          </Typography>
+        </Box>
 
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <IconButton color='inherit' sx={{ mr: 1 }}>
-                        <NotificationsIcon />
-                    </IconButton>
-                    <Avatar alt="Luucianorepetti" src="/placeholder.svg?height=32&width=32" sx={{ backgroundColor: theme.palette.primary.main, width: 40, height: 40, mr: 1 }} />
-                    <Typography variant="body1" noWrap component="div" sx={{ fontSize: "1.2em", display: { xs: 'none', sm: 'block' } }}>
-                        @luucianorepetti
-                    </Typography>
-                </Box>
-            </Toolbar>
-        </AppBar>
-    );
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <IconButton color="inherit" sx={{ mr: 1 }}>
+            <NotificationsIcon />
+          </IconButton>
+          <Avatar
+            alt="Luucianorepetti"
+            src="/placeholder.svg?height=32&width=32"
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              width: 40,
+              height: 40,
+              mr: 1,
+            }}
+          />
+          <Typography
+            variant="body1"
+            noWrap
+            component="div"
+            sx={{ fontSize: "1.2em", display: { xs: "none", sm: "block" } }}
+          >
+            @luucianorepetti
+          </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
