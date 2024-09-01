39e873c881618a6e53d4ba9c1bdc64cc7a801916
Luis Reyes <yuiftw05@gmail.com>
2024-09-01 00:58:21 -0600

    feat: adding mui component and adding styles according to profile design

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -6,9 +6,66 @@ import ShowFollowersBtn from "./profileComponents/ShowFollowersBtn";
 import ShowFollowingBtn from "./profileComponents/ShowFollowingBtn";
 import SendMessageBtn from "./profileComponents/SendMessageBtn";
 
+import SideNavBar from "../shared/SideNavBar";
+
+//? mui material
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
 interface profileProps {
   username: string;
 }
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+  },
+];
 
 export default function Profile({username}: profileProps) {
   return (
@@ -38,6 +95,36 @@ export default function Profile({username}: profileProps) {
         </div>
         
       </div>
+
+      <SideNavBar/>
+
+
+      {/* tercera parte del perfil: botones de redireccion o muestreo: post, pets, about */}
+      {/* div para remarcar las lineas */}
+      <div className="mt-[3rem] border-t-4 border-b-4 border-black">
+        <div className="w-[90%] mx-auto flex justify-between">
+
+        <button className="bg-[red] rounded h-[2rem] w-[3.6rem] m-[.4rem]">posts</button>
+        <button className="bg-[red] rounded h-[2rem] w-[3.6rem] m-[.4rem]">pets</button>
+        <button className="bg-[red] rounded h-[2rem] w-[3.6rem] m-[.4rem]">about</button>
+
+        </div>
+      </div>
+
+      <ImageList sx={{ mt: 4, width: '90%', mx: 'auto'} } cols={3} rowHeight={164}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}
+        >
+          <img            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+            alt={item.title}
+            loading="lazy"
+            className="rounded"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+
     </div>
   )
 }
