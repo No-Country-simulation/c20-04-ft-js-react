494e36c53e3cfa621dbdcb754afd53c0d02af344
Luis Reyes <yuiftw05@gmail.com>
2024-09-03 08:11:52 -0600

    feat: styles: hover effect that displays images name

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import ProfileSettingsBtn from "./profileComponents/ProfileSettingsBtn";
 import EditProfileBtn from "./profileComponents/EditProfileBtn";
@@ -9,8 +9,8 @@ import SendMessageBtn from "./profileComponents/SendMessageBtn";
 import SideNavBar from "../shared/SideNavBar";
 
 //? mui material
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 interface profileProps {
   username: string;
@@ -20,122 +20,134 @@ interface profileProps {
 
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
   },
   {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
   },
   {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
   },
   {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
   },
   {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
   },
   {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
   },
   {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
   },
   {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
   },
   {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
   },
   {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
   },
   {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
   },
   {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
   },
 ];
 
-export default function Profile({username, name, profilePicture,}: profileProps) {
+export default function Profile({
+  username,
+  name,
+  profilePicture,
+}: profileProps) {
   return (
     <div className="flex">
       <div className="z-10 fixed bottom-0 w-[100%] md:static md:bottom-auto md:w-[auto]">
-      <SideNavBar/>
+        <SideNavBar />
       </div>
-          <div className="max-w-[900px] flex-grow w-9/10 mx-auto md:w-full md:mx-0 md:max-w-[none]">
+      <div className="max-w-[900px] flex-grow w-9/10 mx-auto md:w-full md:mx-0 md:max-w-[none]">
+        <div className="flex gap-4 justify-end border-b-[4px] border-b-[#334155] align-center">
+          <p>@{username}</p>
+          <ProfileSettingsBtn />
+          {/* linea que divide la primera parte del perfil */}
+          <div className="border-b-[4px] border-b-[#334155] mt-6"></div>
+        </div>
 
-<div className="flex gap-4 justify-end border-b-[4px] border-b-[#334155] align-center">
-<p>@{username}</p>
-<ProfileSettingsBtn/>
-{/* linea que divide la primera parte del perfil */}
-<div className="border-b-[4px] border-b-[#334155] mt-6"></div>
-</div>
+        {/* segunda parte del perfil */}
+        <div className="mt-5 w-[90%] mx-auto flex justify-between gap-[2rem] max-w-[400px] md:mx-0 md:ml-[3rem] lg:max-w-[550px]">
+          {/* image and name container */}
+          <div className="">
+            <figure className="w-20 h-20 rounded-full bg-[#000]">
+              <img
+                className="w-full h-full rounded-full"
+                src={profilePicture}
+                alt=""
+              />
+            </figure>
+            <p className="max-w-[120px] mt-[0.8rem] overflow-hidden">
+              {name || "user's name"}
+            </p>
+          </div>
 
-{/* segunda parte del perfil */}
-<div className="mt-5 w-[90%] mx-auto flex justify-between gap-[2rem] max-w-[400px] md:mx-0 md:ml-[3rem] lg:max-w-[550px]">
-{/* image and name container */}
-<div className="">
-<figure className="w-20 h-20 rounded-full bg-[#000]">
-  <img className="w-full h-full rounded-full" src={profilePicture} alt="" />
-</figure>
-<p className="max-w-[120px] mt-[0.8rem] overflow-hidden">{name || "user's name"}</p>
-</div>
+          {/* her it goes the follow, following and edit profile/message part */}
+          <div className="flex flex-wrap justify-center items-end gap-4 max-w-[200px] lg:flex-nowrap lg:max-w-[350px]">
+            <ShowFollowersBtn />
+            <ShowFollowingBtn />
+            <EditProfileBtn />
+          </div>
+        </div>
 
-{/* her it goes the follow, following and edit profile/message part */}
-<div className="flex flex-wrap justify-center items-end gap-4 max-w-[200px] lg:flex-nowrap lg:max-w-[350px]">
-  <ShowFollowersBtn/>
-  <ShowFollowingBtn/>
-  <EditProfileBtn/>
-</div>
+        {/* tercera parte del perfil: botones de redireccion o muestreo: post, pets, about */}
+        {/* div para remarcar las lineas */}
+        <div className="mt-[3rem] border-t-4 border-b-4 border-black">
+          <div className="w-[90%] mx-auto flex justify-between lg:justify-start gap-[4rem]">
+            <button className="bg-[red] rounded h-[2rem] w-[3.6rem] m-[.4rem]">
+              posts
+            </button>
+            <button className="bg-[red] rounded h-[2rem] w-[3.6rem] m-[.4rem]">
+              pets
+            </button>
+            <button className="bg-[red] rounded h-[2rem] w-[3.6rem] m-[.4rem]">
+              about
+            </button>
+          </div>
+        </div>
 
-</div>
-
-
-{/* tercera parte del perfil: botones de redireccion o muestreo: post, pets, about */}
-{/* div para remarcar las lineas */}
-<div className="mt-[3rem] border-t-4 border-b-4 border-black">
-<div className="w-[90%] mx-auto flex justify-between lg:justify-start gap-[4rem]">
-
-<button className="bg-[red] rounded h-[2rem] w-[3.6rem] m-[.4rem]">posts</button>
-<button className="bg-[red] rounded h-[2rem] w-[3.6rem] m-[.4rem]">pets</button>
-<button className="bg-[red] rounded h-[2rem] w-[3.6rem] m-[.4rem]">about</button>
-
-</div>
-</div>
-
-<ImageList sx={{ mt: 4, maxWidth: '900px'} } cols={3}>
-{itemData.map((item) => (
-<ImageListItem key={item.img}
-sx={{maxWidth: '320px'}}
->
-  <img            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-    src={`${item.img}`}
-    alt={item.title}
-    loading="lazy"
-    className="rounded"
-  />
-</ImageListItem>
-))}
-</ImageList>
-
-
-</div>
-
+        <ImageList sx={{ mt: 4, maxWidth: "900px" }} cols={3}>
+          {itemData.map((item) => (
+            <ImageListItem
+              key={item.img}
+              sx={{ maxWidth: "320px" }}
+              className="relative group"
+            >
+              <img
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.img}`}
+                alt={item.title}
+                loading="lazy"
+                className="rounded transition-transform duration-200 ease-in-out"
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 rounded">
+                <p className="text-white text-lg">{item.title}</p>
+              </div>
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </div>
     </div>
-
-
-  )
+  );
 }
