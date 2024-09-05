39eb752b17ddf41bb42155642721ea418ccf76e7
Dagadev <gacub20ru@gmail.com>
2024-09-04 23:25:56 -0600

    Add div

--- client/src/app/(main)/layout.tsx
+++ client/src/app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 
-import Header from '@/components/Header/Header'
+import Header from '@/components/header/Header'
 import SideNavBar from '@/components/shared/SideNavBar'
 import type { Metadata } from 'next'
 
@@ -14,9 +14,11 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <main className='min-h-dvh max-w-screen-xl mx-auto flex flex-col'>
       <Header />
-      <div className='flex flex-1 flex-col md:flex-row relative'>
+      <div className='flex flex-col md:flex-row relative'>
         <SideNavBar />
-        {children}
+        <div className='p-5 flex-1 border-r border-neutral-300 dark:border-neutral-300 transition-colors'>
+          {children}
+        </div>
       </div>
     </main >
   )
