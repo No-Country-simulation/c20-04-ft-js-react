559ad08c28fe5486cc1b27f02fa0a0e09e5a0113
luciano-repetti <lucianorepetti2020@gmail.com>
2024-09-06 12:31:32 -0300

    modify form register and controller in back

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -12,8 +12,10 @@ import {
   FormHelperText,
   FormControl,
   CircularProgress,
+  Box,
 } from "@mui/material";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Height, Visibility, VisibilityOff } from "@mui/icons-material";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import Link from "next/link";
 
 //redux
@@ -21,12 +23,17 @@ import { useRegisterMutation } from "@/redux/apiSlices/authApi";
 import { setUser } from "@/redux/slices/userSlice";
 import { useDispatch } from "react-redux";
 import { useRouter } from 'next/navigation'
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import dayjs, { Dayjs } from "dayjs";
+import Alert from "@/components/shared/Alert";
+import { title } from "process";
 
 // Definimos la interfaz para los datos del formulario
 interface FormDataInterface {
   email: string;
   password: string;
   username: string;
+  birthdate: any;
 }
 
 // Definimos la interfaz para los mensajes de error
@@ -34,7 +41,14 @@ interface ErrorFormInterface {
   email: string;
   password: string;
   username: string;
+  birthdate: string;
   terms: string;
+}
+
+interface DataAlertInterface {
+  type: 'info' | 'danger' | 'success' | 'warning' | 'dark';
+  message: string;
+  title: string;
 }
 
 const Input = styled(TextField)(({ theme }) => ({
@@ -57,15 +71,39 @@ const ButtonSubmit = styled(Button)(({ theme }) => ({
   textTransform: "none",
 }));
 
+const CustomDataPicker = styled(DatePicker)(({ theme }) => ({
+  "& .MuiInputBase-root": {
+    width: "100%",
+    height: "50px",
+    borderRadius: "8px",
+  },
+  "& .MuiInputLabel-root": {
+    top: "2px",
+    transform: "translate(16px, 50%)",
+    transition: "all 0.2s ease",
+  },
+  "& .MuiInputLabel-shrink": {
+    top: "3px",
+    transform: "translate(16px, -50%) scale(0.75)",
+  },
+}));
+
 export default function Register() {
   const dispatch = useDispatch();
   const router = useRouter()
 
   // Definimos los estados necesarios
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState<DataAlertInterface>({
+    type: "success",
+    message: "Registro exitoso",
+    title: "Éxito"
+  });
   const [formData, setFormData] = useState<FormDataInterface>({
     email: "",
     username: "",
     password: "",
+    birthdate: null
   });
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [errors, setErrors] = useState<Partial<ErrorFormInterface>>({});
@@ -73,6 +111,13 @@ export default function Register() {
 
   // Hook para la mutación de registro
   const [register, { isLoading, isSuccess, error }] = useRegisterMutation();
+
+  const today = dayjs();
+  const maxDate = today.subtract(16, 'year');
+
+  const handleClose = () => {
+    setShowAlert(false);
+  };
 
   // Manejador para mostrar/ocultar contraseña
   const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -87,6 +132,18 @@ export default function Register() {
     setErrors((prevState) => ({
       ...prevState,
       [id]: "",
+    }));
+  };
+
+  // Manejador para cambios en el DatePicker
+  const handleDateChange = (newDate: Dayjs | null) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      birthdate: newDate,
+    }));
+    setErrors((prevState) => ({
+      ...prevState,
+      birthdate: "",
     }));
   };
 
@@ -108,9 +165,7 @@ export default function Register() {
       newErrors.password = "La contraseña es obligatoria";
     } else if (formData.password.length < 9) {
       newErrors.password = "La contraseña debe tener al menos 9 caracteres";
-    }
-    // Validacion contraseña al menos una mayuscula
-    else if (!/[A-Z]/.test(formData.password)) {
+    } else if (!/[A-Z]/.test(formData.password)) {
       newErrors.password = "La contraseña debe tener al menos una mayúscula";
     }
 
@@ -118,8 +173,12 @@ export default function Register() {
     if (!formData.username) {
       newErrors.username = "El nombre de usuario es obligatorio";
     } else if (formData.username.length < 4) {
-      newErrors.username =
-        "El nombre de usuario debe tener al menos 4 caracteres";
+      newErrors.username = "El nombre de usuario debe tener al menos 4 caracteres";
+    }
+
+    // Validación de la fecha de nacimiento
+    if (!formData.birthdate) {
+      newErrors.birthdate = "La fecha de nacimiento es obligatoria";
     }
 
     // Validamos que se haya aceptado los términos y condiciones
@@ -133,115 +192,174 @@ export default function Register() {
       return;
     }
 
+    // Convertimos la fecha a formato ISO si existe
+    let formattedDate: string | null = null;
+    if (formData.birthdate) {
+      if (dayjs.isDayjs(formData.birthdate)) {
+        formattedDate = formData.birthdate.toISOString();
+      } else {
+        // Si `formData.birthdate` es una cadena, podemos asignarla directamente
+        formattedDate = formData.birthdate;
+      }
+    }
+
+    // Creamos una copia de los datos del formulario con la fecha formateada
+    const dataToSend: FormDataInterface = {
+      ...formData,
+      birthdate: formattedDate, // Enviamos la fecha en formato ISO
+    };
+
     // Llamamos a la función de registro
-    postRegister(formData);
+    postRegister(dataToSend);
   };
 
   // Función asíncrona para registrar al usuario
   const postRegister = async (dataForm: FormDataInterface) => {
     try {
       const response = await register(dataForm).unwrap();
-      dispatch(setUser(response));
-      router.push('/home')
+
+      // Verificar la respuesta de la API
+      if (response.code === 201 && response.status === "success") {
+        dispatch(setUser(response.data));
+        router.push('/home');
+      } else if (response.code === 409) {
+        setAlertMessage({
+          type: "danger",
+          message: response.message,
+          title: "Error"
+        });
+        setShowAlert(true);
+      }
     } catch (err) {
-      console.error("Error al registrarse:", err);
+      setAlertMessage({
+        type: "danger",
+        message: "al registrarse. Intenta nuevamente.",
+        title: "Error"
+      });
+      setShowAlert(true);
     }
   };
 
   return (
-    <div className="bg-[#CEC5FD] dark:bg-[#674cf0] min-h-screen flex items-center justify-center p-4">
-      <div className="bg-white dark:bg-neutral-900 rounded-lg shadow-md max-w-md w-full p-8">
-        <div className="text-center mb-6">
-          <h2 className="text-2xl font-semibold text-gray-800 dark:text-neutral-100">
-            Bienvenido a la comunidad Pawpal
-          </h2>
-          <p className="text-sm text-gray-600 dark:text-neutral-200 mt-2">
-            Registrate para compartir tus mascotas!
-          </p>
-        </div>
-        <form className="space-y-4" onSubmit={handleSubmit}>
-          <Input
-            type="text"
-            label="Nombre de usuario"
-            variant="outlined"
-            id="username"
-            value={formData?.username}
-            onChange={handleInputChange}
-            className="w-full dark:bg-neutral-800 rounded-lg"
-            error={!!errors.username}
-            helperText={errors.username}
-          />
-          <Input
-            type="email"
-            label="Correo electrónico"
-            variant="outlined"
-            id="email"
-            value={formData?.email}
-            onChange={handleInputChange}
-            className="w-full dark:bg-neutral-800 rounded-lg"
-            error={!!errors.email}
-            helperText={errors.email}
-          />
-          <Input
-            type={showPassword ? "text" : "password"}
-            label="Contraseña"
-            id="password"
-            className="w-full dark:bg-neutral-800 rounded-lg"
-            value={formData?.password}
-            onChange={handleInputChange}
-            error={!!errors.password}
-            helperText={errors.password}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-          <FormControl error={!!errors.terms} component="fieldset">
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    id="termsAccepted"
-                    checked={termsAccepted}
-                    onChange={() => setTermsAccepted(!termsAccepted)}
-                  />
-                }
-                label="Acepto los términos y condiciones"
-              />
-            </FormGroup>
-            {errors.terms && <FormHelperText>{errors.terms}</FormHelperText>}
-          </FormControl>
-
-          <ButtonSubmit
-            type="submit"
-            className="w-full bg-[#A14CEB] hover:bg-[#8A3CD1] text-white"
-            disabled={isLoading}
-          >
-            {isLoading ? (
-              <CircularProgress size={24} sx={{ color: "white" }} />
-            ) : (
-              "Registrarse"
-            )}
-          </ButtonSubmit>
-          <div className="mt-6 text-center">
-            <p className="text-sm text-gray-600 dark:text-neutral-100">
-              ¿Ya tienes una cuenta?
-              <Link href="/login" className="text-[#A14CEB] ml-1">
-                Iniciar sesión
-              </Link>
+    <>
+      <div className="bg-[#CEC5FD] dark:bg-[#674cf0] min-h-screen flex items-center justify-center p-4">
+        <div className="bg-white dark:bg-neutral-900 rounded-lg shadow-md max-w-md w-full p-8">
+          <div className="text-center mb-6">
+            <h2 className="text-2xl font-semibold text-gray-800 dark:text-neutral-100">
+              Bienvenido a la comunidad Pawpal
+            </h2>
+            <p className="text-sm text-gray-600 dark:text-neutral-200 mt-2">
+              Registrate para compartir tus mascotas!
             </p>
           </div>
-        </form>
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <Input
+              type="text"
+              label="Nombre de usuario"
+              variant="outlined"
+              id="username"
+              value={formData?.username}
+              onChange={handleInputChange}
+              className="w-full dark:bg-neutral-800 rounded-lg"
+              error={!!errors.username}
+              helperText={errors.username}
+            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <CustomDataPicker
+                label="Fecha de nacimiento"
+                value={formData?.birthdate} // Enlazamos la fecha seleccionada al estado
+                onChange={handleDateChange} // Manejamos el cambio de fecha
+                maxDate={maxDate} // Restringimos la selección de fechas a mayores de 16 años
+                format="DD/MM/YYYY"
+                className="w-full dark:bg-neutral-800 rounded-lg"
+                slotProps={{
+                  textField: {
+                    error: !!errors.birthdate,
+                    helperText: errors.birthdate,
+                  },
+                }}
+              />
+            </LocalizationProvider>
+            <Input
+              type="email"
+              label="Correo electrónico"
+              variant="outlined"
+              id="email"
+              value={formData?.email}
+              onChange={handleInputChange}
+              className="w-full dark:bg-neutral-800 rounded-lg"
+              error={!!errors.email}
+              helperText={errors.email}
+            />
+            <Input
+              type={showPassword ? "text" : "password"}
+              label="Contraseña"
+              id="password"
+              className="w-full dark:bg-neutral-800 rounded-lg"
+              value={formData?.password}
+              onChange={handleInputChange}
+              error={!!errors.password}
+              helperText={errors.password}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <FormControl error={!!errors.terms} component="fieldset">
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      id="termsAccepted"
+                      checked={termsAccepted}
+                      onChange={() => setTermsAccepted(!termsAccepted)}
+                    />
+                  }
+                  label="Acepto los términos y condiciones"
+                />
+              </FormGroup>
+              {errors.terms && <FormHelperText>{errors.terms}</FormHelperText>}
+            </FormControl>
+
+            <ButtonSubmit
+              type="submit"
+              className="w-full bg-[#A14CEB] hover:bg-[#8A3CD1] text-white"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <CircularProgress size={24} sx={{ color: "white" }} />
+              ) : (
+                "Registrarse"
+              )}
+            </ButtonSubmit>
+            <div className="mt-6 text-center">
+              <p className="text-sm text-gray-600 dark:text-neutral-100">
+                ¿Ya tienes una cuenta?
+                <Link href="/login" className="text-[#A14CEB] ml-1">
+                  Iniciar sesión
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+      {showAlert && (
+        <Alert
+          type={alertMessage.type}
+          title={alertMessage.title}
+          message={alertMessage.message}
+          onClose={handleClose}
+        />
+      )}
+    </>
   );
 }
---
+++ client/src/components/shared/Alert.tsx
@@ -0,0 +1,90 @@
+// components/Alert.tsx
+import React, { useEffect, useState } from 'react';
+
+interface AlertProps {
+    type: 'info' | 'danger' | 'success' | 'warning' | 'dark';
+    title: string;
+    message: string;
+    onClose: () => void; // Añadido para manejar el cierre del alerta
+}
+
+const alertStyles = {
+    info: {
+        text: 'text-blue-800',
+        border: 'border-blue-300',
+        bg: 'bg-blue-50',
+        darkText: 'dark:text-blue-400',
+        darkBorder: 'dark:border-blue-800',
+        iconPath: 'M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z',
+    },
+    danger: {
+        text: 'text-red-800',
+        border: 'border-red-300',
+        bg: 'bg-red-50',
+        darkText: 'dark:text-red-400',
+        darkBorder: 'dark:border-red-800',
+        iconPath: 'M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z',
+    },
+    success: {
+        text: 'text-green-800',
+        border: 'border-green-300',
+        bg: 'bg-green-50',
+        darkText: 'dark:text-green-400',
+        darkBorder: 'dark:border-green-800',
+        iconPath: 'M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z',
+    },
+    warning: {
+        text: 'text-yellow-800',
+        border: 'border-yellow-300',
+        bg: 'bg-yellow-50',
+        darkText: 'dark:text-yellow-300',
+        darkBorder: 'dark:border-yellow-800',
+        iconPath: 'M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z',
+    },
+    dark: {
+        text: 'text-gray-800',
+        border: 'border-gray-300',
+        bg: 'bg-gray-50',
+        darkText: 'dark:text-gray-300',
+        darkBorder: 'dark:border-gray-600',
+        iconPath: 'M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z',
+    },
+};
+
+const Alert: React.FC<AlertProps> = ({ type, title, message, onClose }) => {
+    const { text, border, bg, darkText, darkBorder, iconPath } = alertStyles[type];
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsVisible(false);
+            setTimeout(onClose, 300); // Espera a que la animación de salida termine
+        }, 5000); // Duración visible del alert en milisegundos
+
+        return () => clearTimeout(timer); // Limpia el temporizador si el componente se desmonta
+    }, [onClose]);
+
+    return (
+        <div
+            className={`fixed top-4 right-4 flex items-center p-4 text-sm ${text} border ${border} rounded-lg ${bg} dark:bg-gray-800 ${darkText} dark:border-gray-800 transition-transform transform ${isVisible ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0'
+                }`}
+            role="alert"
+        >
+            <svg
+                className="flex-shrink-0 inline w-4 h-4 me-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+            >
+                <path d={iconPath} />
+            </svg>
+            <span className="sr-only">{type.charAt(0).toUpperCase() + type.slice(1)}</span>
+            <div>
+                <span className="font-medium">{title}</span> {message}
+            </div>
+        </div>
+    );
+};
+
+export default Alert;
