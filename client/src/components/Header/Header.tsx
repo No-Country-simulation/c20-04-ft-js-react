1da63fc174d5c5478e7cfc3ab2a6937f93396fdf
Dagadev <gacub20ru@gmail.com>
2024-09-06 12:48:37 -0600

    Update styles

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -8,29 +8,24 @@ import {
   Typography,
   Avatar,
   Box,
-  ThemeProvider,
 } from "@mui/material";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { useTheme } from "@mui/material/styles";
-import { useSelector } from 'react-redux';
-import { RootState } from '@/redux/store';
-import Link from '@mui/material/Link';
 import PawPalIcon from "@/icons/PawPal";
+import { useAppSelector } from "@/redux/hooks";
+import SearchIcon from "@/icons/Search";
+import { stringAvatar } from "@/utils/avatar";
 
 export default function Header() {
   const theme = useTheme();
-  const user = useSelector((state: RootState) => state.userReducer.user)
-  const username = user?.username
+  const user = useAppSelector(state => state.userReducer.user)
 
   return (
     <AppBar
       position="sticky"
-      // color="default"
+      color="inherit"
       elevation={0}
       sx={{
-        // position: "sticky",
-        top: 0,
-        zIndex: "100",
         display: "flex",
         justifyContent: "center",
         border: `1px solid ${theme.palette.divider}`,
@@ -48,28 +43,36 @@ export default function Header() {
           </Typography>
         </Box>
 
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <IconButton color="inherit" sx={{ mr: 1 }}>
-            <NotificationsIcon />
+        <Box className='flex items-center gap-x-2 md:gap-x-4'>
+          <IconButton color="inherit">
+            <NotificationsIcon className='size-8' />
           </IconButton>
-          <Avatar
-            alt={username?.toUpperCase()}
-            src="/placeholder.svg?height=32&width=32"
-            sx={{
-              backgroundColor: theme.palette.primary.main,
-              width: 40,
-              height: 40,
-              mr: 1,
-            }}
-          />
-          <Typography
-            variant="body1"
-            noWrap
-            component="div"
-            sx={{ fontSize: "1.2em", display: { xs: "none", sm: "block" } }}
-          >
-            @{username?.toLowerCase()}
-          </Typography>
+          <IconButton color="inherit" className='md:hidden'>
+            <SearchIcon />
+          </IconButton>
+          <Box className='items-center gap-x-3 hidden md:flex'>
+            <Box>
+              <Typography
+                variant="body1"
+                noWrap
+                className='text-end font-semibold'
+              >
+                User Name
+              </Typography>
+              <Typography
+                variant="body1"
+                noWrap
+                className="text-neutral-700 dark:text-neutral-300 text-sm text-end"
+              >
+                @{user?.username.toLowerCase()}
+              </Typography>
+            </Box>
+            <Avatar
+              {...stringAvatar('Nombre nombre')}
+              alt={user?.username.toUpperCase()}
+              className='size-12'
+            />
+          </Box>
         </Box>
       </Toolbar>
     </AppBar>
