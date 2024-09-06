e3816b937ee1a225d1977bc81b5c807c1d56d09b
Dagadev <gacub20ru@gmail.com>
2024-09-05 23:48:23 -0600

    Add fill icon

---
+++ client/src/icons/ChatBoxFill.tsx
@@ -0,0 +1,17 @@
+export default function ChatBoxFillIcon({ ...props }: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      width='32'
+      height='32'
+      viewBox='0 0 32 32'
+      fill='currentColor'
+      stroke='currentColor'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M9 29a1 1 0 0 1-1-1v-4H6.5A4.505 4.505 0 0 1 2 19.5v-12A4.505 4.505 0 0 1 6.5 3h19A4.505 4.505 0 0 1 30 7.5v12a4.505 4.505 0 0 1-4.5 4.5H15.359L9.64 28.768c-.18.15-.407.232-.641.232Z'
+      />
+    </svg>
+  )
+}
