1e8b550f4dfff23ebcf1480aebe3cb457ac4a1c1
Dagadev <gacub20ru@gmail.com>
2024-09-06 12:04:58 -0600

    Add avatar utils

---
+++ client/src/utils/avatar.ts
@@ -0,0 +1,28 @@
+// Crea un color hexadecimal en base al nombre del usuario
+function stringToColor(string: string) {
+  let hash = 0;
+  let i;
+
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+
+  return color;
+}
+
+// Devuleve configuracion para el componente avatar de MUI como un color de fondo y la iniciales del nombre
+export function stringAvatar(name: string) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  }
+}
