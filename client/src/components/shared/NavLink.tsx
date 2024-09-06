05ba1ec7a7c48722975bf980c8e507f29b44e4ba
Dagadev <gacub20ru@gmail.com>
2024-09-06 12:04:41 -0600

    Add NavLink component

---
+++ client/src/components/shared/NavLink.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function NavLink({ link, pathname, className, children }: {
+  link: {
+    href: string
+    name: string
+  }
+  pathname: string
+  className?: string
+  children: React.ReactNode
+}) {
+  return (
+    <Link
+      href={'/' + link.href}
+      className={
+        'flex gap-x-4 py-2 px-4 items-center font-semibold rounded-md md:rounded-full md:border border-transparent transition md:hover:text-black md:dark:hover:text-white md:hover:border-neutral-300 md:dark:hover:border-neutral-700 ' +
+        (pathname.includes(link.href) ? 'text-black dark:text-white ' : 'text-neutral-400 dark:text-neutral-500 ') + className ?? ''
+      }
+    >
+      {children}
+      <span className='hidden md:inline'>{link.name}</span>
+    </Link>
+  )
+}
