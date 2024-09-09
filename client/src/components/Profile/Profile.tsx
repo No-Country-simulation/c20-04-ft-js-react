1d6c0af3a8cc02fda6b9bdf7c7f7363b7e153cbe
Luis Reyes <yuiftw05@gmail.com>
2024-09-09 10:16:42 -0600

    feat: final EditProfileBasicInfoLayout done and responsive design working. Getting name in redux petition for profile

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -23,7 +23,6 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import { Box } from "@mui/material";
 
-
 interface profileProps {
   dataUsername: string;
   paramsUsername: string;
@@ -65,53 +64,55 @@ export default function Profile({
 
   return (
     <div className=" relative max-w-full flex-grow w-9/10 mx-auto md:w-full md:mx-0 md:max-w-[none]">
-      
-      <div className="mt-5 w-[95%] mx-auto flex justify-between gap-[2rem] max-w-[400px] md:mx-0 md:ml-[3rem] lg:max-w-[650px]">
       {/* segunda parte del perfil */}
-        {editFlag ? (
-          <EditProfileBasicInfoLayout username={dataUsername} name={name} profilePicture={profilePicture} editFlag={editFlag} setEditFlag={setEditFlag}/>
-        ) : (
+      {editFlag ? (
+        <EditProfileBasicInfoLayout
+          username={dataUsername}
+          name={name}
+          profilePicture={profilePicture}
+          editFlag={editFlag}
+          setEditFlag={setEditFlag}
+        />
+      ) : (
+        <div className="mt-5 w-[95%] flex max-w-[400px] mx-auto justify-between gap-[2rem] md:mx-0 md:ml-[3rem] lg:max-w-[650px]">
           <div className="lg:flex gap-[1rem] min-w-[120px] relative">
-          <figure className="w-20 h-20 rounded-full bg-[#000]">
-            <img
-              className="w-full h-full rounded-full"
-              src={profilePicture}
-              alt=""
-            />
-          </figure>
-          <p className="max-w-[120px] mt-[0.8rem] overflow-hidden font-extrabold text-[1rem] lg:text-[1.8rem] lg:max-w-[220px] lg:mt-0">
-            {name}
-          </p>
-          <p className="lg:w-[6.4rem] lg:h-[3rem] absolute bottom-[-15%] lg:left-[35%]">
-            @{dataUsername}
-          </p>
+            <figure className="w-20 h-20 rounded-full bg-[#000]">
+              <img
+                className="w-full h-full rounded-full object-cover"
+                src={profilePicture}
+                alt=""
+              />
+            </figure>
+            <p className="max-w-[120px] mt-[0.8rem] overflow-hidden font-extrabold text-[1rem] lg:text-[1.8rem] lg:max-w-[220px] lg:mt-0">
+              {name}
+            </p>
+            <p className="lg:w-[6.4rem] lg:h-[3rem] absolute bottom-[-15%] lg:left-[35%]">
+              @{dataUsername}
+            </p>
+          </div>
+
+          <div className="flex flex-wrap justify-center items-end gap-4 max-w-[200px] lg:flex-nowrap lg:max-w-[350px]">
+            {editFlag ? (
+              <></>
+            ) : dataUsername === localUsername ? (
+              <>
+                <ShowFollowersBtn />
+                <ShowFollowingBtn />
+                <SendNewProfileInfo
+                  editFlag={editFlag}
+                  setEditFlag={setEditFlag}
+                />
+              </>
+            ) : (
+              <div className="flex gap-[1rem] w-[100%]">
+                <FollowOrUnfollowBtn />
+                <SendMessageBtn />
+              </div>
+            )}
+          </div>
         </div>
-        )}
+      )}
 
-        {/* her it goes the follow, following and edit profile/message part */}
-        <div className="flex flex-wrap justify-center items-end gap-4 max-w-[200px] lg:flex-nowrap lg:max-w-[350px]">
-  {editFlag ? (
-    <>
-      
-    </>
-  ) : dataUsername === localUsername ? (
-    <>
-      <ShowFollowersBtn />
-      <ShowFollowingBtn />
-      <SendNewProfileInfo editFlag={editFlag} setEditFlag={setEditFlag} />
-    </>
-  ) : (
-    <div className="flex gap-[1rem] w-[100%]">
-      <FollowOrUnfollowBtn />
-      <SendMessageBtn />
-    </div>
-  )}
-</div>
-
-      </div>
-
-      {/* tercera parte del perfil: botones de redireccion o muestreo: post, pets, about */}
-      {/* div para remarcar las lineas */}
       <div className="mt-[3rem] border-t-2 border-[#e2e5e9]">
         <div className="w-[90%] mx-auto flex justify-between lg:justify-start gap-[4rem] m-[.8rem]">
           <button
@@ -145,11 +146,11 @@ export default function Profile({
       <Box
         gap={4}
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '100% ',
+          display: "flex",
+          flexDirection: "column",
+          width: "100% ",
         }}
-        className='p-2 md:p-[50px]'
+        className="p-2 md:p-[50px]"
       >
         {renderSelectedComponent()}
       </Box>
--- client/src/components/Profile/profileComponents/EditProfileBasicInfoLayout.tsx
+++ client/src/components/Profile/profileComponents/EditProfileBasicInfoLayout.tsx
@@ -32,26 +32,24 @@ export default function EditProfileBasicInfoLayout({
   const handleProfilePictureUpload = () => {};
 
   return (
-    <div className="flex w-[100%] flex-col items-center self-center justify-self-center gap-[1rem]">
+    <div className="flex w-full flex-col items-center self-center justify-self-center gap-4 lg:flex-row">
       {/* Profile Picture Section */}
-
-      <div className="flex flex-col gap-[1rem]">
-        {/* Este input será invisible pero funcional */}
+      <div className="relative flex flex-col gap-4">
+        {/* Hidden input for profile picture upload */}
         <input
           type="file"
           accept="image/*"
-          onChange={(e) => handleProfilePictureUpload()}
-          className="hidden" // Ocultamos el input
+          onChange={handleProfilePictureUpload}
+          className="hidden"
           id="profile-picture-upload"
         />
 
-        {/* Figura que actuará como el botón visible */}
+        {/* Label that acts as visible button */}
         <label
-          htmlFor="profile-picture-upload" // Vinculamos la figura con el input
-          className="w-20 h-20 rounded-full border-2 border-red-500 cursor-pointer flex justify-center items-center"
-          style={{ backgroundColor: "transparent" }}
+          htmlFor="profile-picture-upload"
+          className="w-20 h-20 rounded-full border-2 border-red-500 cursor-pointer flex justify-center items-center relative"
         >
-          {/* Imagen o ícono por defecto */}
+          {/* Display profile picture or default upload text */}
           {profilePicture ? (
             <img
               className="w-full h-full rounded-full object-cover"
@@ -59,15 +57,22 @@ export default function EditProfileBasicInfoLayout({
               alt="Profile Picture"
             />
           ) : (
-            <span className="text-red-500">Upload</span> // Texto o ícono alternativo si no hay imagen
+            <span className="text-red-500">Upload</span>
           )}
+
+          {/* Text overlay */}
+          <span className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-sm rounded-full text-center">
+            Tap here to change PFP
+          </span>
         </label>
       </div>
 
-      <div className="text-center">
-        <div className="mb-[1.2rem]">
-          <label htmlFor="newName" className="text-[1.2rem] font-extrabold">
-            Current name: {name}
+      {/* Form Section */}
+      <div className="flex flex-col gap-4 text-center lg:text-left lg:flex-row">
+        {/* Name Input */}
+        <div className="mb-4">
+          <label htmlFor="newName" className="text-xl font-extrabold">
+            Current Name: {name}
           </label>
           <input
             type="text"
@@ -77,28 +82,27 @@ export default function EditProfileBasicInfoLayout({
               setDataToUpdate({ ...dataToUpdate, newName: e.target.value })
             }
             placeholder="Enter new name"
-            className="w-[90%]  h-[60px] max-w-[320px] text-center  overflow-hidden font-extrabold text-[1rem] lg:text-[1.8rem] lg:max-w-[220px] lg:mt-0 border border-[#8c52ff] rounded"
+            className="w-[90%] h-[60px] max-w-[320px] text-center font-extrabold text-lg border border-[#8c52ff] rounded lg:text-2xl lg:max-w-[220px]"
           />
         </div>
 
         {/* Username Input */}
-        <div>
-          <label htmlFor="newUsername" className="text-[1.2rem] font-extrabold">
-            Current username: {username}
+        <div className="mb-4">
+          <label htmlFor="newUsername" className="text-xl font-extrabold">
+            Current Username: {username}
           </label>
           <input
             type="text"
             name="newUsername"
-            value={username}
+            value={dataToUpdate.newUsername}
             onChange={(e) =>
               setDataToUpdate({ ...dataToUpdate, newUsername: e.target.value })
             }
             placeholder="Enter new username"
-            className="lg:w-[6.4rem] lg:h-[3rem] text-center border border-[#8c52ff] rounded w-[90%] h-[60px] max-w-[320px]"
+            className="w-[90%] h-[60px] max-w-[320px] text-center font-extrabold text-lg border border-[#8c52ff] rounded lg:text-2xl lg:max-w-[220px]"
           />
         </div>
       </div>
-
       <SendNewProfileInfo editFlag={editFlag} setEditFlag={setEditFlag} />
     </div>
   );
