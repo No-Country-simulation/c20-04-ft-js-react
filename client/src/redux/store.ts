11c184b35a8e490c8ec880436727eaa7995312f3
Dagadev <gacub20ru@gmail.com>
2024-09-09 19:50:37 -0600

    Add search slice

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -4,24 +4,31 @@ import { configureStore } from '@reduxjs/toolkit'
 import { authApi } from './apiSlices/authApi'
 import { userApi } from './apiSlices/userApi'
 import { userQueryApi } from './apiSlices/userQueryApi'
+import { searchQueryApi } from './apiSlices/searchQueryApi'
 
 //import de los slices van aca:
 import userSlice from "./slices/userSlice"
 import themeSlice from './slices/themeSlice'
+import searchSlice from './slices/searchSlice'
 
 const store = configureStore({
     reducer: {
         // los que manejan el contenido y estado global
         userReducer: userSlice,
         theme: themeSlice,
+        search: searchSlice,
 
         // los que son para peticiones
         [authApi.reducerPath]: authApi.reducer,
         [userApi.reducerPath]: userApi.reducer,
-        [userQueryApi.reducerPath]: userQueryApi.reducer
+        [userQueryApi.reducerPath]: userQueryApi.reducer,
+        [searchQueryApi.reducerPath]: searchQueryApi.reducer
     },
     //concatenamos los apiSlices que usamos para hacer las peticiones a nuestro backend
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware, authApi.middleware, userQueryApi.middleware,)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
+        userApi.middleware, authApi.middleware,
+        userQueryApi.middleware, searchQueryApi.middleware
+    )
 })
 
 // typescript necesita el root state para poder saber que tipo de valores estamos asignando en nuestro estado global, de otra forma, no podra leerlos
