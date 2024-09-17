a44fbfef21e0e28739859b7321b9dc9b06d7d275
luciano-repetti <lucianorepetti2020@gmail.com>
2024-09-17 00:30:27 -0300

    fix get post username and add loading fetching posts

--- client/src/components/Profile/profileComponents/ProfilePosts.tsx
+++ client/src/components/Profile/profileComponents/ProfilePosts.tsx
@@ -8,7 +8,7 @@ import ImageListItem from "@mui/material/ImageListItem";
 import type { Post as PostType } from '@/types'
 import { useGetAllPostByUsernameQuery } from '@/redux/apiSlices/postApi';
 import { useParams } from 'next/navigation';
-import { Box } from '@mui/material';
+import { Box, CircularProgress } from '@mui/material';
 
 export default function ProfilePosts() {
   const [posts, setPosts] = useState<PostType[]>([])
@@ -18,11 +18,12 @@ export default function ProfilePosts() {
   const { data, isError, isLoading } = useGetAllPostByUsernameQuery(username)
 
   useEffect(() => {
-    if (isLoading == false) {
-      console.log(data);
-      setPosts(data.data.getAllPostByUsername)
+    if (!isLoading && data) {
+      console.log(data); // Verifica la estructura de data
+      const fetchedPosts = data.data?.getAllPostByUsername || [];
+      setPosts(fetchedPosts);
     }
-  }, [data, isLoading])
+  }, [data, isLoading]);
 
   const addNewPost = (newPost: Omit<PostType, 'comments' | 'likes' | ''>) => {
     setPosts((ps) => [...ps, { ...newPost, comments: 0, likes: 0 }])
@@ -30,19 +31,36 @@ export default function ProfilePosts() {
 
   return (
     <>
-      {data?.length ? (
-        posts.toReversed().map((post) => (
-          <Post
-            key={post.id}
-            post={post}
-          />
-        ))
-      ) : (
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <p className="mb-[1rem] font-extrabold text-[1rem] lg:text-[1.4rem]">
-            No hay publicaciones disponibles.
-          </p>
+      {isLoading ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          position="fixed"
+          top="0"
+          left="0"
+          width="100%"
+          height="100%"
+          bgcolor="background.paper" // Fondo blanco para el contenedor de carga
+          zIndex="tooltip"
+        >
+          <CircularProgress />
         </Box>
+      ) : (
+        posts.length > 0 ? (
+          posts.toReversed().map((post) => (
+            <Post
+              key={post.id}
+              post={post}
+            />
+          ))
+        ) : (
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <p className="mb-[1rem] font-extrabold text-[1rem] lg:text-[1.4rem]">
+              No hay publicaciones disponibles.
+            </p>
+          </Box>
+        )
       )}
     </>
   )
